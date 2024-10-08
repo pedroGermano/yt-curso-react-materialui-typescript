eff57db64284d7b006ad5c82d04fa6f7cf65b34d
Pedro Germano <pedrogermano232@gmail.com>
2024-10-08 14:17:46 -0300

    FEAT: Material UI add

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { Route, Routes, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/home" element={<p>Página inicial</p>} />
+      <Route path="/home" element={<Button>Teste</Button>} />
       <Route path="*" element={<Navigate to="/home" />} />
     </Routes>
   );
