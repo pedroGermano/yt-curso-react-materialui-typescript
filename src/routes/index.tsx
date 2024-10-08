62c4fe1b0ba2976fe47e8d441d91e0f4c5f468cb
Pedro Germano <pedrogermano232@gmail.com>
2024-10-08 13:09:08 -0300

    FEAT: add react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export {};
+import { Route, Routes, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/home" element={<p>Página inicial</p>} />
+      <Route path="*" element={<Navigate to="/home" />} />
+    </Routes>
+  );
+};
