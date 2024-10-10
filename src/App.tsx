c47ae185bf86969fd9b91ea338e05c8c3f0025bd
Pedro Germano <pedrogermano232@gmail.com>
2024-10-10 08:59:39 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import { BrowserRouter } from "react-router-dom";
-import { AppRoutes } from "./routes";
-import { ThemeProvider } from "@mui/material";
-import { LightTheme } from "./utils/themes";
+import { BrowserRouter } from 'react-router-dom';
+
+import './shared/forms/TraducoesYup';
+
+import { AppThemeProvider, AuthProvider, DrawerProvider } from './shared/contexts';
+import { Login, MenuLateral } from './shared/components';
+import { AppRoutes } from './routes';
+
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </ThemeProvider>
+    <AuthProvider>
+      <AppThemeProvider>
+
+        <Login>
+
+          <DrawerProvider>
+            <BrowserRouter>
+
+              <MenuLateral>
+                <AppRoutes />
+              </MenuLateral>
+
+            </BrowserRouter>
+          </DrawerProvider>
+
+        </Login>
+
+      </AppThemeProvider>
+    </AuthProvider>
   );
 };
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,5 @@
-export {};
+export * from './pessoas/ListagemDePessoas';
+export * from './pessoas/DetalheDePessoas';
+export * from './cidades/ListagemDeCidades';
+export * from './cidades/DetalheDeCidades';
+export * from './dashboard/Dashboard';
